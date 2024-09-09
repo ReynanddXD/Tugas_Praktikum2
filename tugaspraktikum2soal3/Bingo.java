43dd2f6b4e9cda505fc20883a4721c44eb1ba3d4
ReynanddXD <165867557+ReynanddXD@users.noreply.github.com>
2024-09-09 12:28:56 +0700

    Add files via upload

---
+++ tugaspraktikum2soal3/Bingo.java
@@ -0,0 +1,55 @@
+package tugaspraktikum2soal3;
+
+class Bingo {
+    String clap;
+
+    // Konstruktor
+    public Bingo(String clap) {
+        this.clap = clap;
+    }
+    public void laguBingo(){
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println("B-I-N-G-O");
+        System.out.println("B-I-N-G-O");
+        System.out.println("B-I-N-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+        
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println(clap+"-I-N-G-O");
+        System.out.println(clap+"-I-N-G-O");
+        System.out.println(clap+"-I-N-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+        
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println(clap+"-"+clap+"-N-G-O");
+        System.out.println(clap+"-"+clap+"-N-G-O");
+        System.out.println(clap+"-"+clap+"-N-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+        
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println(clap+"-"+clap+"-"+clap+"-G-O");
+        System.out.println(clap+"-"+clap+"-"+clap+"-G-O");
+        System.out.println(clap+"-"+clap+"-"+clap+"-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+        
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println(clap+"-"+clap+"-"+clap+"-"+clap+"-O");
+        System.out.println(clap+"-"+clap+"-"+clap+"-"+clap+"-O");
+        System.out.println(clap+"-"+clap+"-"+clap+"-"+clap+"-O");
+        System.out.println("And Bingo was his name-o.\n");
+        
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println(clap+"-"+clap+"-"+clap+"-"+clap+"-"+clap);
+        System.out.println(clap+"-"+clap+"-"+clap+"-"+clap+"-"+clap);
+        System.out.println(clap+"-"+clap+"-"+clap+"-"+clap+"-"+clap);
+        System.out.println("And Bingo was his name-o.\n");
+    }
+
+   
+}
---
+++ tugaspraktikum2soal3/BingoKonstruktor.java
@@ -0,0 +1,11 @@
+package tugaspraktikum2soal3;
+
+public class BingoKonstruktor {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bingo bingo = new Bingo("(CLAP)");
+        bingo.laguBingo();
+    }
+    
+}
